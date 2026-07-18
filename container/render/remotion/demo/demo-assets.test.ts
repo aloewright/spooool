42e21a9ae6b4b4f1dab7a3283414c8e58294f577
Aloe <aloe@Aloes-MacBook-Air.local>
2026-07-18 12:25:37 -0700

    test: lock demo screen asset mappings

--- container/render/remotion/demo/demo-assets.test.ts
+++ container/render/remotion/demo/demo-assets.test.ts
@@ -3,14 +3,14 @@ import { DEMO_ASSETS, getRequiredDemoAssets } from "./demo-assets";
 
 describe("demo assets", () => {
   it("declares every approved product moment and both audio beds", () => {
-    expect(Object.keys(DEMO_ASSETS.screens)).toEqual([
-      "home",
-      "compose",
-      "outline",
-      "editor",
-      "book",
-      "publish",
-    ]);
+    expect(DEMO_ASSETS.screens).toEqual({
+      home: "demo/screens/studio-home.png",
+      compose: "demo/screens/studio-compose.png",
+      outline: "demo/screens/studio-outline.png",
+      editor: "demo/screens/studio-editor.png",
+      book: "demo/screens/studio-book.png",
+      publish: "demo/screens/studio-publish.png",
+    });
     expect(DEMO_ASSETS.audio).toEqual({
       landscape: "demo/audio/spooool-demo-landscape.wav",
       vertical: "demo/audio/spooool-demo-vertical.wav",
